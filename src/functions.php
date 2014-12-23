a8a9a82a3cec544e1f6551b17b2097693c367fe0
BogdanWDK <pletea66@gmail.com>
2014-12-23 15:39:18 +0200

    Update #3
    Function for adfly short url api.

--- src/functions.php
+++ src/functions.php
@@ -47,4 +47,29 @@ function message($cli) {
   }
 }
 
+
+
+function short_url($url, $key, $uid, $domain = 'adf.ly', $advert_type = 'int')
+{
+  // base api url
+  $api = 'http://api.adf.ly/api.php?';
+
+  // api queries
+  $query = array(
+    'key' => $key,
+    'uid' => $uid,
+    'advert_type' => $advert_type,
+    'domain' => $domain,
+    'url' => $url
+  );
+
+  // full api url with query string
+  $api = $api . http_build_query($query);
+  // get data
+  if ($data = file_get_contents($api))
+    return $data;
+}
+
+
+
 ?>
