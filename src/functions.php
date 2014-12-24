aa4294b4fdd3e78045e3062fab20bd43d88a419d
BogdanWDK <pletea66@gmail.com>
2014-12-24 02:14:24 +0200

    Update #5
    Random string function added.

--- src/functions.php
+++ src/functions.php
@@ -70,6 +70,15 @@ function short_url($url, $key, $uid, $domain = 'adf.ly', $advert_type = 'int')
     return $data;
 }
 
+function generateRandomString($length) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
 
 
 ?>
