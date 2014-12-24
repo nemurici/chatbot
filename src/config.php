a6a7314f5fd13e689572cc04270520658b6c9fed
nemurici <lucy_neda95@yahoo.com>
2014-12-24 02:52:20 +0200

    Update #6

--- src/config.php
+++ src/config.php
@@ -1,8 +1,8 @@
 <?php
 
 $admin[] = "em";
-$pubfnc = array("whois", "ping", "host","b64_encode","b64_decode","md5_encode","sha1_encode","short","passwd_generate");
-$privfnc = array("sh", "msg");
+$pubfnc = array("whois", "ping", "host","b64_encode","b64_decode","md5_encode","sha1_encode","short","passwd_generate","_banned");
+$privfnc = array("sh", "msg","_ban","_unban");
 
 # Configure chat user and pass
 
