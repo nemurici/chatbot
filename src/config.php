3c35f0dbe9cff9d03c3ee7a2756ace65082e9971
BogdanWDK <pletea66@gmail.com>
2014-12-24 02:15:46 +0200

    Update #5
    Password generate command added.

--- src/config.php
+++ src/config.php
@@ -1,7 +1,7 @@
 <?php
 
 $admin[] = "em";
-$pubfnc = array("whois", "ping", "host","b64_encode","b64_decode","md5_encode","sha1_encode","short");
+$pubfnc = array("whois", "ping", "host","b64_encode","b64_decode","md5_encode","sha1_encode","short","passwd_generate");
 $privfnc = array("sh", "msg");
 
 # Configure chat user and pass
